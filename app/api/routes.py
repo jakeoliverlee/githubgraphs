04bad628ff92f41c0c33b700cb1631c7857819b7
Jake <jakeoliverlee@gmail.com>
2023-05-15 15:53:23 +0100

    add theme option to commitgraph

--- app/api/routes.py
+++ app/api/routes.py
@@ -4,20 +4,25 @@ from app.services.commitgraph import fetch_commit_count_per_day, plot_commit_cou
 api = Blueprint("api", __name__)
 
 
-# http://example/v1/commit-graph?username={username}&repo={repo}&type={svg|png}
+# http://example/v1/commit-graph?username={username}&repo={repo}&type={svg|png}&theme={dark|light}
 @api.route("/v1/commit-graph", methods=["GET"])
 def get_commit_graph():
     username = request.args.get("username", default=None, type=str)
     repo = request.args.get("repo", default=None, type=str)
     type = request.args.get("type", default=None, type=str)
+    theme = request.args.get("theme", default=None, type=str)
     if username:
         try:
             commit_count = fetch_commit_count_per_day(username, repo)
             if type == "png":
-                plot_commit_count(commit_count, f"{repo}_commit_graph.png", repo, "png")
+                plot_commit_count(
+                    commit_count, f"{repo}_commit_graph.png", repo, "png", theme
+                )
                 return send_file(f"{repo}_commit_graph.png", mimetype="image/png")
             else:  # Default to SVG if type is not specified or not "png"
-                plot_commit_count(commit_count, f"{repo}_commit_graph.svg", repo, "svg")
+                plot_commit_count(
+                    commit_count, f"{repo}_commit_graph.svg", repo, "svg", theme
+                )
                 return send_file(f"{repo}_commit_graph.svg", mimetype="image/svg+xml")
 
         except ValueError as e:
--- app/services/commitgraph.py
+++ app/services/commitgraph.py
@@ -7,6 +7,65 @@ from scipy.interpolate import make_interp_spline, BSpline
 import numpy as np
 from scipy.interpolate import PchipInterpolator
 
+THEMES = {
+    "dark": {
+        "style": "dark_background",
+        "line_color": "dodgerblue",
+        "fill_color": "dodgerblue",
+        "fill_alpha": 0.2,
+        "label_color": "white",
+        "tick_color": "white",
+    },
+    "light": {
+        "style": "seaborn-whitegrid",
+        "line_color": "lightblue",
+        "fill_color": "lightblue",
+        "fill_alpha": 0.2,
+        "label_color": "black",
+        "tick_color": "black",
+    },
+    "sunset": {
+        "style": "seaborn-darkgrid",
+        "line_color": "darkorange",
+        "fill_color": "gold",
+        "fill_alpha": 0.3,
+        "label_color": "black",
+        "tick_color": "black",
+    },
+    "forest": {
+        "style": "seaborn-darkgrid",
+        "line_color": "darkgreen",
+        "fill_color": "lightgreen",
+        "fill_alpha": 0.2,
+        "label_color": "black",
+        "tick_color": "black",
+    },
+    "ocean": {
+        "style": "seaborn-darkgrid",
+        "line_color": "darkblue",
+        "fill_color": "lightblue",
+        "fill_alpha": 0.2,
+        "label_color": "black",
+        "tick_color": "black",
+    },
+    "sakura": {
+        "style": "seaborn-whitegrid",
+        "line_color": "deeppink",
+        "fill_color": "lightpink",
+        "fill_alpha": 0.2,
+        "label_color": "black",
+        "tick_color": "black",
+    },
+    "monochrome": {
+        "style": "seaborn-whitegrid",
+        "line_color": "black",
+        "fill_color": "gray",
+        "fill_alpha": 0.2,
+        "label_color": "black",
+        "tick_color": "black",
+    },
+}
+
 
 def fetch_commit_count_per_day(owner, repo):
     base_url = f"https://api.github.com/repos/{owner}/{repo}/commits"
@@ -42,8 +101,13 @@ def fetch_commit_count_per_day(owner, repo):
     return commit_count
 
 
-def plot_commit_count(commit_count, filename, repo, file_format):
-    plt.style.use("dark_background")
+def plot_commit_count(commit_count, filename, repo, file_format, theme):
+    theme_settings = THEMES.get(theme, THEMES["dark"])  # Use dark theme as default
+    plt.rcdefaults()  # reset matplotlib settings to default.
+    plt.style.use(theme_settings["style"])
+
+    plt.figure(figsize=(12, 6))  # Create a new figure with specified width and height
+
     dates = sorted(commit_count.keys())
     counts = [commit_count[date] for date in dates]
 
@@ -57,15 +121,20 @@ def plot_commit_count(commit_count, filename, repo, file_format):
     pchip = PchipInterpolator(date_nums, counts)
     smooth_counts = pchip(t)
 
-    plt.figure(figsize=(12, 6))  # Create a new figure with specified width and height
-
     # Plot the smooth curve
-    plt.plot(t, smooth_counts, color="dodgerblue")
+    plt.plot(t, smooth_counts, color=theme_settings["line_color"])
 
-    plt.plot(date_nums, counts, marker="o", linestyle="", color="dodgerblue")
+    plt.plot(
+        date_nums, counts, marker="o", linestyle="", color=theme_settings["line_color"]
+    )
 
-    # Fill the area below the curve with blue color
-    plt.fill_between(t, smooth_counts, color="dodgerblue", alpha=0.2)
+    # Fill the area below the curve with the chosen color
+    plt.fill_between(
+        t,
+        smooth_counts,
+        color=theme_settings["fill_color"],
+        alpha=theme_settings["fill_alpha"],
+    )
 
     # Configure the x-axis
     plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%d"))
@@ -73,11 +142,14 @@ def plot_commit_count(commit_count, filename, repo, file_format):
         mdates.DayLocator(interval=3)
     )  # Set x-axis ticks to one every three days
 
-    plt.xlabel("Days", color="white")
-    plt.ylabel("Commit Count", color="white")
-    plt.title(f"Commit Count for {repo}", color="white")
-    plt.xticks(rotation=45, color="white")
-    plt.yticks(color="white")
+    plt.xlabel("Days", color=theme_settings["label_color"])
+    plt.ylabel("Commit Count", color=theme_settings["label_color"])
+    plt.title(f"Commit Count for {repo}", color=theme_settings["label_color"])
+    plt.xticks(rotation=45, color=theme_settings["tick_color"])
+    plt.yticks(color=theme_settings["tick_color"])
 
     # Save the plot to a file
     plt.savefig(filename, format=file_format, dpi=1200)
+
+    # Clear the current figure to ensure that the next plot does not inherit the settings of the previous plot
+    plt.clf()
